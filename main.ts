e02cae1e914d52ceb8d860bee5180d5ea957db43
hohoihung <w10546.ho@gmail.com>
2023-02-14 20:30:31 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -40,7 +40,33 @@ control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EV
         basic.clearScreen()
     }
 })
+serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
+    voice_id = serial.readLine()
+    basic.showString(voice_id)
+    if (voice_id == "3") {
+        wuKong.mecanumRun(wuKong.RunList.Front, 49)
+    } else if (voice_id == "4") {
+        wuKong.mecanumRun(wuKong.RunList.rear, 49)
+    } else if (voice_id == 6) {
+        wuKong.mecanumRun(wuKong.RunList.left, 49)
+    } else if (voice_id == 7) {
+        wuKong.mecanumRun(wuKong.RunList.right, 49)
+    } else if (voice_id == 6) {
+        wuKong.mecanumSpin(wuKong.TurnList.Left, 50)
+    } else if (voice_id == 7) {
+        wuKong.mecanumSpin(wuKong.TurnList.Right, 50)
+    } else if (voice_id == 8) {
+        wuKong.mecanumDrift(wuKong.TurnList.Left)
+        wuKong.mecanumDrift(wuKong.TurnList.Right)
+    } else if (voice_id == 5) {
+        wuKong.mecanumStop()
+    } else {
+        basic.showNumber(voice_id)
+        basic.clearScreen()
+    }
+})
 let obstacle_disc = 0
+let voice_id = ""
 wuKong.mecanumWheel(
 wuKong.ServoList.S2,
 wuKong.ServoList.S3,
@@ -48,6 +74,11 @@ wuKong.ServoList.S0,
 wuKong.ServoList.S1
 )
 wuKong.mecanumStop()
+serial.redirect(
+SerialPin.P2,
+SerialPin.P1,
+BaudRate.BaudRate9600
+)
 basic.showIcon(IconNames.Heart)
 basic.forever(function () {
     obstacle_disc = sonar.ping(
