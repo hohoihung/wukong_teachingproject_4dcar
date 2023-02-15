2b9a9c6930d9106c9087b1bc27ed2507ff93c0d4
hohoihung <w10546.ho@gmail.com>
2023-02-15 13:04:52 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 control.onEvent(EventBusSource.MES_DEVICE_INFO_ID, EventBusValue.MICROBIT_EVT_ANY, function () {
+    basic.showIcon(IconNames.SmallHeart)
     if (in_motion) {
         wuKong.mecanumSpin(wuKong.TurnList.Left, 80)
         basic.pause(200)
@@ -32,7 +33,6 @@ input.onButtonPressed(Button.B, function () {
     wuKong.mecanumStop()
     in_motion = 0
 })
-let obstacle_disc = 0
 let voice_id = ""
 let in_motion = 0
 radio.setGroup(8)
@@ -48,22 +48,5 @@ wuKong.setLightMode(wuKong.LightMode.OFF)
 let strip = neopixel.create(DigitalPin.P12, 4, NeoPixelMode.RGB)
 basic.showIcon(IconNames.Heart)
 basic.forever(function () {
-    obstacle_disc = sonar.ping(
-    DigitalPin.P14,
-    DigitalPin.P15,
-    PingUnit.Centimeters
-    )
-    if (obstacle_disc < 25) {
-        control.raiseEvent(
-        EventBusSource.MES_DEVICE_INFO_ID,
-        EventBusValue.MICROBIT_EVT_ANY
-        )
-    }
-    if (pins.digitalReadPin(DigitalPin.P0) == 1) {
-        wuKong.lightIntensity(100)
-        strip.showRainbow(1, 360)
-    } else {
-        wuKong.lightIntensity(0)
-        strip.showColor(neopixel.colors(NeoPixelColors.Black))
-    }
+	
 })
